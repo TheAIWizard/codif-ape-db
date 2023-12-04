4edce54b11d07f56f6d5d844692107366e939e5b
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2023-12-04 17:33:45 +0000

    reste ajout var categorielles pr avoir même nettoyé libellé et comparer probas

--- get-model.sh
+++ get-model.sh
@@ -1,4 +1,5 @@
 # get FastText-APE artifacts 
+mc cp --recursive s3/projet-ape/mlflow-artifacts/6/d22c02c2df384c549c315b49af338988/artifacts/default/artifacts/default.bin FastText-APE/v6 
 mc cp --recursive s3/projet-ape/mlflow-artifacts/6/525ef98699b14f0586f5b0eed2cdd79e/artifacts/default/artifacts/default.bin FastText-APE/v3  
 mc cp --recursive s3/projet-ape/mlflow-artifacts/6/4e5c4673cbbd412e91456b3443e8dabe/artifacts/default/artifacts/default.bin FastText-APE/v2
 mc cp --recursive s3/projet-ape/mlflow-artifacts/1/5490ebb3b62a43e494517f819cf20322/artifacts/default/artifacts/default.bin FastText-APE/v1
--- test.py
+++ test.py
@@ -5,7 +5,28 @@ db = pd.read_parquet("db-sirene-2023/base_sirene_ape_2023.parquet")
 
 db_auto=db[db["mode_calcul_ape"]=="AUTO"]
 
-db_final=predict(db_auto.head(50), version="v3")[["liasse_numero","apet_finale","apet_1_nace","mode_calcul_ape"]]
+# Définir la plage de dates à tester (avril à juin 26)
+date_debut = pd.Timestamp('2023-06-27 18:20:43', tz='UTC')
+date_fin = pd.Timestamp('2023-03-01', tz='UTC')
+
+# Filtrer les lignes où la date_modification est comprise entre avril et le 26 juin
+db_auto_periode = db_auto[(db_auto['date_modification'] >= date_debut)] #& (db_auto['date_modification'] <= date_fin)]
+
+db_final=predict(db_auto_periode.head(5000), version="v6")[["liasse_numero","apet_finale","apet_1_nace","mode_calcul_ape","date_modification"]]
 
 print(db_final)
 
+# Sélectionner les lignes où les valeurs de 'apet_finale' et 'apet_1_nace' diffèrent
+result = db_final[db_final['apet_finale'] != db_final['apet_1_nace']]
+
+# Afficher le résultat avec les dates correspondantes
+print(result[['liasse_numero', 'apet_finale', 'apet_1_nace', 'mode_calcul_ape', 'date_modification']])
+
+# Assurez-vous que la colonne date_modification est de type datetime
+result['date_modification'] = pd.to_datetime(result['date_modification'])
+
+# Extraire les mois de la colonne date_modification
+mois_uniques = result['date_modification'].dt.month.unique()
+
+# Afficher l'ensemble des mois uniques
+print(mois_uniques)
